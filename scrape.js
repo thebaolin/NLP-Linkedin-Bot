f06bfd545db64a4af2df96e31d93cc0d84b19f4f
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-09-24 09:10:50 -0400

    there are most certainly packages here that I don't need

---
+++ scrape.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getPostTitles = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://old.reddit.com/r/programming/'
+		);
+		const $ = cheerio.load(data);
+		const postTitles = [];
+
+		$('div > p.title > a').each((_idx, el) => {
+			const postTitle = $(el).text()
+			postTitles.push(postTitle)
+		});
+
+		return postTitles;
+	} catch (error) {
+		throw error;
+	}
+};
+
+getPostTitles().then((postTitles) => console.log(postTitles));
+
