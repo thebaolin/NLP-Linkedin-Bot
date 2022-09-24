1fb01ce402e6fdc464868cee5f0ffdd792f9437e
Jerry Zhu <xpost2000cod@gmail.com>
2022-09-24 13:27:39 -0400

    some crap, also this is the wrong branch fuuuuu

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 import { useState } from 'react';
-const cohere = require('cohere-ai')
 
+const axios = require('axios');
 const log_entry = (name, text) => [name, text];
 // DUM DUM DUM DUM
 const API_KEY = "fcCuE2KiwJqdaTYEoR2Al9RaVT13ltr32eo2cIw0";
-cohere.init(API_KEY); // bad
+
+const HEADER_FOR_REQUESTS = {
+  headers: {
+  'content-type': 'application/json',
+  'authorization': `BEARER ${API_KEY}`
+  }
+};
+async function cohere_generate(params) {
+  const result = await axios.post("https://api.cohere.ai/generate", params, HEADER_FOR_REQUESTS);
+  return result.data;
+}
 
 /* function cohere_generate(gcc)
  *  */
-function SearchBar({add_output}) {
+function SearchBar({logstuff, add_output, update_output}) {
   const [search_string, update_search_string] = useState("");
 
   return (
     <div className="center-this">
       <div className="search-bar">
         <input type="text"
+               value={search_string}
                onChange={
                function(event) {
                  update_search_string(event.target.value);
@@ -25,11 +36,23 @@ function SearchBar({add_output}) {
                }
                placeholder="Enter a LinkedIn URL..."></input>
       </div>
-      <button onClick={() => add_output(log_entry("User", search_string))}>
+      <button onClick={
+      async function() {
+        {
+          const prompt = search_string;
+          let current_output = add_output(logstuff, log_entry("User", prompt)) ;
+          update_output(current_output);
+          update_search_string("");
+          const lol = await cohere_generate({prompt: prompt});
+          current_output = add_output(current_output, log_entry("Bot", `${prompt}${lol.text}`));
+          console.log(current_output);
+          update_output(current_output);
+        }
+      }}>
         Enter
       </button>
     </div>
-  )
+        )
 }
 
 function TextLine(context) {
@@ -53,15 +76,15 @@ function App() {
   ]);
 
   // garbage collector will fix this later...
-  function add_output(entry) {
-    const new_text_log = [entry, ...text_line_log];
-    update_text_line_log(new_text_log);
+  function add_output(old, entry) {
+    const new_text_log = [entry, ...old];
+    return new_text_log;
   }
 
   return (
     <div className="App">
       <OutputBox output={text_line_log} />
-      <SearchBar add_output={add_output} />
+      <SearchBar logstuff={text_line_log} add_output={add_output} update_output={update_text_line_log} />
     </div>
   );
 }
