f641ba92b6acc9fd18ffdf4e9d866ae709434801
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-09-24 02:21:08 -0400

    I like things to work

--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
 import requests
-from . import parse
+import parse
 
 res = requests.get('https://www.linkedin.com/jobs/view/mechanical-technician-1-at-northrop-grumman-3285901132?trk=org-job-results')
 
