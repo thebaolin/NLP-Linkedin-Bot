945dc9194e86627f8aec8b16d4069341f03a72cb
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-09-24 02:07:12 -0400

    fuck you jerry

---
+++ test.py
@@ -0,0 +1,6 @@
+import requests
+
+res = requests.get('https://www.linkedin.com/jobs/view/mechanical-technician-1-at-northrop-grumman-3285901132?trk=org-job-results')
+
+print(res.text)
+print(res.status_code)
