ec29653bfe44f832bee550fc449de705d2fb3707
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-09-24 14:44:46 -0400

    something that might be working the way I want and I don't want to lose

--- scrape.js
+++ scrape.js
@@ -6,12 +6,14 @@ const cheerio = require('cheerio');
 const Cohere = require("cohere-ai");
 Cohere.init("fcCuE2KiwJqdaTYEoR2Al9RaVT13ltr32eo2cIw0");
 
+let URL = "https://www.linkedin.com/jobs/view/mechanical-technician-1-at-northrop-grumman-3285901132?trk=org-job-results"
+
 const getJobDesc = async (url) => {
 	try {
 		const { data } = await axios.get(url);
 		const $ = cheerio.load(data);
-
-		return $("div.show-more-less-html__markup").text();
+	
+		return $("div.show-more-less-html__markup").text().split(".");
 	} catch (error) {
 		throw error;
 	}
@@ -24,5 +26,9 @@ const parseKW = async (jobDesc) => {
 	return `${keyWords.body.generations[0].text}`;
 }
 
-//getJobDesc("https://www.linkedin.com/jobs/view/mechanical-technician-1-at-northrop-grumman-3285901132?trk=org-job-results").then((jobDesc) => console.log(jobDesc));
-parseKW('string').then((Prediction) => console.log('We have a fun time here'+Prediction));
+;(async () => {
+const s = await getJobDesc(URL);	
+for(let i =0;i<s.length;i++){
+	console.log(s[i]);
+}
+})();
