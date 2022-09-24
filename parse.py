287f2fb20a58ae28a77687898e72a59d47f69f46
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-09-24 02:35:51 -0400

    time to learn a new language. Maybe french, possibly javascript

--- parse.py
+++
@@ -1,4 +0,0 @@
-def Parse(res):
-    print(res.text)
-    print(res.status_code)
-
--- scrape.py
+++
@@ -1,6 +0,0 @@
-import requests
-import parse
-
-res = requests.get('https://www.linkedin.com/jobs/view/mechanical-technician-1-at-northrop-grumman-3285901132?trk=org-job-results')
-
-parse.Parse(res)
