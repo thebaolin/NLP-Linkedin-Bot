9fe4ff0f5a6df5dba8385d6e0b3fddbe3a33bab5
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-09-24 20:22:30 -0400

    Let's Save!

--- scrape.js
+++ scrape.js
@@ -1,6 +1,7 @@
 //Importing axios and cheerio
 const axios = require('axios');
 const cheerio = require('cheerio');
+// const fs = require('fs');
 
 //Setting up Cohere
 const Cohere = require("cohere-ai");
@@ -9,8 +10,9 @@ Cohere.init("fcCuE2KiwJqdaTYEoR2Al9RaVT13ltr32eo2cIw0");
 //IOstream
 const fs = require("fs").promises;
 
-let URL = "https://www.linkedin.com/jobs/view/3178546564/?alternateChannel=search&refId=uLeACUbPD1ZRMi5Ux1pIuQ%3D%3D&trackingId=jR6UMddU%2B7BEEKgS0YyGPg%3D%3D";
-let oFile = "tData/trainAP1";
+let URL = "https://www.linkedin.com/jobs/view/mechanical-technician-1-at-northrop-grumman-3285901132?trk=org-job-results";
+
+let iFile = "tData/trainNG";
 
 const getJobDesc = async (url) => {
 	try {
@@ -25,21 +27,55 @@ const getJobDesc = async (url) => {
 
 //req_ex = //list of tuples, training data
 
-const parseKW = async (jobDesc) => {
-	const keyWords = await Cohere.generate({prompt: 'We have a fun time here' });
+const parseKW = async (jobDesc, training) => {
+	const keyWords = await Cohere.generate({
+		model: "large",
+		prompt: promptGen(training,jobDesc),
+		max_tokens: 10,
+		temperature: 0.1,
+		stop_sequences: ["\n"]
+	});
 	return `${keyWords.body.generations[0].text}`;
 }
 
+function zip(a, b) {
+	let result = [];
+	
+	for (let i = 0; i < a.length; ++i) {
+		result.push([a[i], b[i]]);
+	}
+
+	return result;
+}
+
+const promptGen = (a, ex, fd) => {
+	exam = [...a[0], ...[ex]];
+	label = [...a[1], ...[""]];
+	const full = zip(exam,label);
+	return full.map(([examp, label]) => ("\n---\n"+examp+"\n"+fd+label));
+}
+
+fs.readFile(iFile, 'utf8', (err, data) => {
+	if(err) {
+	} else {
+		const training_data = JSON.parse(data);
+		console.log(training_data);
+	}
+});
+
 ;(async () => {
-	const s = await getJobDesc(URL);
+//	const s = await getJobDesc(URL);
 
 	try {
-		await fs.writeFile(oFile, URL+"\n");
+//		await fs.writeFile(oFile, URL+"\n");
 		
-		for(let i =0;i<s.length;i++){
-			await fs.appendFile(oFile, "{\"\", \""+s[i]+"\"}\n");
-		}
+//		for(let i =0;i<s.length;i++){
+			
+//			await fs.appendFile(oFile, "{\"\", \""+s[i]+"\"}\n");
+//		}
 	}catch (error) {
 		throw error;
 	}
 })();
+
+
